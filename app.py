79e795901f0630548c32812c58d560ce7210fd3c
joaohsilva <joaohasilva416@gmail.com>
2026-04-26 13:09:28 -0300

    feat: adiciona função add_task com persistência em JSON

--- app.py
+++ app.py
@@ -6,7 +6,31 @@ from datetime import datetime
 
 # Função para adicionar tarefa
 def add_task():
-    pass
+    command = sys.argv[2] # Comando para o CLI
+    # Verifica se o arquivo json existe
+    if os.path.exists('tasks.json'):
+        # Se existir abre o arquivo e carrega as tarefas
+        with open("tasks.json", "r") as file:
+            tasks = json.load(file)
+    
+    # Se não existir inicia uma lista vazia
+    else:
+        tasks = []
+    
+    # Abre o arquivo JSON para escrita
+    with open("tasks.json", "w") as file:
+        # Adiciona a tarefa na lista de tarefas
+        tasks.append({
+            "id": len(tasks) + 1,
+            "description": command,
+            "status": "todo",
+            "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "updatedAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        })
+
+        # Escreve as tarefas no arquivo JSON
+        json.dump(tasks, file, indent=4)
+        print(f"Task added succesfully (ID: {len(tasks)})")
 
 
 # Função para atualizar tarefa
@@ -44,3 +68,6 @@ commands = {
     "mark-done": mark_task_as_done,
     "mark-in-progress": mark_task_as_in_progress,
 }
+
+operation_function = commands[command]
+operation_function()
